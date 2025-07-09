6bbcd257d6570e3c2ba73780a38ff3b44fe4e755
kaname4455 <kanamebaseball2721@gmail.com>
2025-07-09 21:25:23 +0900

    ReviewForm.javaを最新化

--- samuraitravel/src/main/java/com/example/samuraitravel/form/ReviewForm.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/form/ReviewForm.java
@@ -4,11 +4,9 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class ReviewForm {
     @NotBlank(message = "レビューを入力して下さい")
     private String review;
