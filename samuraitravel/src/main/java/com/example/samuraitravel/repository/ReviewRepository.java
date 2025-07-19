1d7cccc19b4f43c9279e5ad9270dbdddbfc8c188
kaname4455 <kanamebaseball2721@gmail.com>
2025-07-20 01:16:16 +0900

    ReviewRepository.javawo

--- samuraitravel/src/main/java/com/example/samuraitravel/repository/ReviewRepository.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/repository/ReviewRepository.java
@@ -2,12 +2,13 @@ package com.example.samuraitravel.repository;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.samuraitravel.entity.House;
 import com.example.samuraitravel.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
-	   public List<Review> findTop6ByHouseOrderByCreatedAtDesc(House house);
+    List<Review> findByHouse(House house);
+
+	List<Review> findTop6ByHouseOrderByCreatedAtDesc(House house);
 }
