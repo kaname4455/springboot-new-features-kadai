42ba0563ddc9b4e4106936c00caf9518ea59990d
kaname4455 <kanamebaseball2721@gmail.com>
2025-07-20 01:15:16 +0900

    HousereviewRepository.javawo

--- samuraitravel/src/main/java/com/example/samuraitravel/repository/HousereviewRepository.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/repository/HousereviewRepository.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.samuraitravel.entity.House;
 import com.example.samuraitravel.entity.Review;
 
 //HousereviewRepository.java
 @Repository
-public interface HousereviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findByHouseEntityId(Integer houseId);
+public interface HousereviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findByHouse(House house);
 }
 
 
